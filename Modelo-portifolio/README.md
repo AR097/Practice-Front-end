e2cbd1bc0ead022a24828da2b3830482e0af5298
Andressa Rodrigues <107823438+AR097@users.noreply.github.com>
2024-05-31 15:02:09 -0300

    Add files via upload

---
+++ Modelo-portifolio/README.md
@@ -0,0 +1,22 @@
+# Portifólio
+
+Site portifólio responsivo desenvolvido com react e vite.
+
+## Start and Run:
+
+Clone this repositorie: <br>
+  ```sh
+  git clone https://github.com/AR097/Portifolio.git
+  ```
+Install folders and libraries: <br> 
+  ```sh
+ npm install
+  ```
+Include the extension vite :
+  ```sh
+vite
+  ```
+![image](https://github.com/AR097/Portifolio/assets/107823438/5df5b492-cd38-4ad2-bc18-4579faed8044)
+
+Run: <br>
+`port http://localhost:4000/`
---
+++ Modelo-portifolio/src/pgs/SobreOProjeto/Sobre.tsx
@@ -0,0 +1,8 @@
+Seja bem vindo!
+Estamos desenvolvendo um espaço web para unir quem tem vontade de ajudar e que gostaria mas não entende do assunto. 
+Não prestamos, oferecemos ou realizamos qualque evento presencial, arrecadações de dinheiro ou qualquer outra forma de aceitamento de doaçoes. A ideia aqui é que possamos facilitar a abrangencia de noticias, e pedidos de ajuda para que alcanncem o maior numero de pessoas possiveis, a fim de ampliar nosso grupo de voluntarios pelo brasil e pelo mundo.
+Aqui, Acreditamos que um pequeno gesto pode fazer a diferença. 
+
+Minha ideia aqui ainda nao esta tão clara mas quero alguns pontos:
+- Ser um app com uso minimo de memoria e super leve para dispositiveis moveis. 
+- Poder futuramente permitir que as noticias e pedidos sejam realiados em tempo real, e o monitoramento de possiveis tragedias seja monitorado para que ficamos atentos.
